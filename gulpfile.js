73ccba003725b16b77bbcd3d49941205a418ffde
Andrey <homandrej@gmail.com>
2016-04-07 17:08:40 +0300

    Added gulp into Binovery_site

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp'),
+	concatCss = require('gulp-concat-css'),
+	cssmin = require('gulp-minify-css'),
+	rename = require('gulp-rename'),
+    notify = require('gulp-notify'),
+    sass = require('gulp-sass');
+    // stylus = require('gulp-stylus');
+
+gulp.task('css', function () {
+  return gulp.src('./public/css/*.css')
+    .pipe(concatCss("bundle.css"))
+    .pipe(cssmin())
+    .pipe(rename('bundle.min.css'))
+    .pipe(notify('Done!'))
+    .pipe(gulp.dest('./public/build/'));
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./public/scss/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./public/css/*.css', ['css'])
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./public/scss/*.scss', ['sass', 'css']);
+});
+
+// gulp.task('styl', function() {
+//     return gulp.src('./public/stylus/*.styl')
+//         .pipe(stylus({
+//             linenos: false
+//         }))
+        
+//         .pipe(concatCss('styl.css'))
+//         .pipe(gulp.dest('./public/build/')); 
+
+// });
